fceda40daebe64ba9974562a1a01598f4cc30da6
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-01-16 22:17:26 +0800

    Create #蒙特卡罗方法计算圆周率.py

---
+++ Python北京理工大学MOOC/Python语言程序设计/#蒙特卡罗方法计算圆周率.py
@@ -0,0 +1,15 @@
+#CalpiV2.py
+#蒙特卡罗方法计算圆周率
+from random import random
+from time import perf_counter
+DARTS = 1000*10000
+hits = 0
+start = perf_counter()
+for i in range(1,DARTS+1):
+    x,y=random(),random()
+    dist = pow(x**2+y**2,0.5)
+    if dist <=1.0:
+        hits = hits+1
+pi = 4*(hits/DARTS)
+print("圆周率的值为：{:.50f}".format(pi))
+print("运行时间为：{:.10f}s".format(perf_counter()-start))
