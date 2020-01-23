f014d3c62ef1be98c0ea3c0296f06b482cd33f6f
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-01-23 08:49:41 +0800

    Create #DayDayUpQ4.py

---
+++ Python北京理工大学MOOC/Python语言程序设计/#DayDayUpQ4.py
@@ -0,0 +1,13 @@
+#DayDayUpQ4.py
+def dayup(df):
+    dayup = 1
+    for i in range(365):
+        if i % 7 in [6,0]:
+            dayup = dayup * (1-0.01)
+        else:
+            dayup = dayup * (1+df)
+    return dayup
+dayfactor = 0.01
+while dayup(dayfactor) < 37.78:
+    dayfactor += 0.001
+print("工作日努力的参数为:{:.3f}".format(dayfactor))
