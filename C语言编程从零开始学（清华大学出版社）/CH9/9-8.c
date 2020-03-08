268b3f8e5bc8916f604bc61256fd42edd3af7093
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-08 09:25:56 +0000

    Create 9-8.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH9/9-8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int *fun(int x, int y)
+{
+    if(x>y)
+    {
+        return &x;
+    }
+    else
+    {
+        return &y;
+    }
+}
+int main()
+{
+    int a,b;
+    int *p;
+    printf("请输入两个整数:\n");
+    scanf("%d%d", &a, &b);
+    p = fun(a,b);
+    printf("指针变指向最大值为:%d\n", *p);
+    return 0;
+}
