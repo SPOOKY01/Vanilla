c2afa322c14d5910b75fcabb8bbed6e014eaad97
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-08 09:27:59 +0000

    Create 9-10.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH9/9-10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5] = {99,37,100,82,252};
+    int i;
+    printf("数组arr中的元素为:\n");
+    for(i = 0;i<5;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
