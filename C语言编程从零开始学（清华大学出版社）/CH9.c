9e3ab05f6f71cbf72af79b79ff70224e9c1c8f37
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-08 09:09:38 +0000

    Update CH9.c

--- C语言编程从零开始学（清华大学出版社）/CH9.c
+++ C语言编程从零开始学（清华大学出版社）/CH9.c
@@ -1,20 +1,3 @@
-#include<stdio.h>//9-1
-int main()
-{
-    int a,*p;
-    p = &a;
-    a = 10;
-    printf("a:%d\n",a);
-    printf("*p:%d\n",*p);
-    printf("&a:%x\n",&a);
-    printf("p:%x\n",p);
-    printf("&p:%x\n",&p);
-    return 0;
-}
-
-
-
-
 #include<stdio.h>//9-2
 int main()
 {
