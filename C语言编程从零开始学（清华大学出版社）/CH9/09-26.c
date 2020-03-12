52bdf962c94330e3b67c17ad0474a1afbea5cbd7
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-12 03:52:50 +0000

    Update 09-26.c

--- C语言编程从零开始学（清华大学出版社）/CH9/09-26.c
+++ C语言编程从零开始学（清华大学出版社）/CH9/09-26.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
-void fun()
+void fun(char *p1,char *p2)
+{
+    for(;*p1!='\0';p1++,p2++)
+    {
+        *p1=*p2;
+    }
+    *p1 = '\0';
+}
+int main()
+{
+    char a1[10]="abc";
+    char a2[10]="ABCDE";
+    printf("输入字符串 a1=%s\n", a1);
+    printf("输入字符串 a2=%s\n", a2);
+    fun(a1, a2);
+    printf("复制后\n");
+    printf("字符串 a1=%s\n", a1);
+    printf("字符串 a2=%s\n", a2);
+    return 0;
+}
