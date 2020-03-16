1847b3ba6f070c49b0f56605794da774c8e9d4df
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-16 22:40:20 +0800

    Create 04-03.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH04/04-03.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char a='Y';
+	char b='y';
+	char c='>';
+	char d='9';
+	printf("%c 的ASCII码值为: %d\n",a,a);
+	printf("%c 的ASCII码值为: %d\n",b,b);
+	printf("%c 的ASCII码值为: %d\n",c,c);
+	printf("%c 的ASCII码值为: %d\n",d,d);
+	return 0;
+}
