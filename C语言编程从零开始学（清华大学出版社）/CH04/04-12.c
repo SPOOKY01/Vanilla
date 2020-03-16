502e38c4fdae866e8c3280c61cd70b4de69d0160
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-16 22:47:21 +0800

    Create 04-12.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH04/04-12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float f=0.618f;                    //定义  单精度类型变量
+	double d=25.9637;                  //定义  双精度类型变量
+	long double ld=7.943852;           //定义长双精度类型变量
+	printf("单精度类型变量f=%f\n",f);
+	printf("双精度类型变量d=%f\n",d);
+	printf("长双精度类型变量ld=%f\n",ld);
+	return 0;
+}
