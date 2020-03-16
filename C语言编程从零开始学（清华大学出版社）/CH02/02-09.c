24119e9fa5376bea2d5f6d4ba18423cf5e06daf4
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-16 20:44:53 +0800

    Create 02-09.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH02/02-09.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2,ch3;//定义字符型变量
+	ch1='N';
+	ch2='e';
+	ch3='w';
+	putchar(ch1);
+	putchar(ch2);
+	putchar(ch3);
+	putchar(' ');
+	putchar('y');
+	putchar('e');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
+}
