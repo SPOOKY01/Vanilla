b5d3dade13a24e39e747040a8ac9bb5920197081
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-16 23:12:33 +0800

    Create 06-01.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH06/06-01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	int b;
+	a = 'b';
+	b = 111;//给字符型变量b赋值小写字母o的ASCII码
+	putchar(a);//按顺序输出字符串boy
+	putchar('\n');
+	putchar(b);
+	putchar('\n');
+	putchar('y');
+	putchar('\n');
+	return 0;
+}
