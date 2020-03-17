ff49fddee36de2ed87d6cbeeafe24397309f7448
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 17:06:41 +0800

    Create 07-12.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH07/07-12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fun(int a, int b);
+int main()
+{
+    int a = 7;
+    int b = 2;
+    printf("参数交换之前 a = %d, b = %d\n", a, b);
+    fun(a, b);
+    return (0);//
+}
+void(int a, int b)
+{
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+    printf("参数交换之后 a = %d, b = %d\n", a, b);
+}
