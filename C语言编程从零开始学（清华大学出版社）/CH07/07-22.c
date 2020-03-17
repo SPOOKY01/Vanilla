47f42bd6baad9c1dc1aa2410604360ecf0362523
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 20:26:31 +0800

    Create 07-22.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH07/07-22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fun(int n, int i, int j, int k)
+{
+    printf("请输入: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            k = i*j;
+            printf("%d*%dx=%2d  ", j, i, k);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main()
+{
+    int  a = 0,b = 0,c = 0,d = 0,t;
+    t = fun(a,b,c,d);
+    return 0;
+}
