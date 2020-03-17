80448529988d5c773fcd5f92ebdb0dd572a3aa17
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 14:18:51 +0800

    Update 06-16.c

--- C语言编程从零开始学（清华大学出版社）/CH06/06-16.c
+++ C语言编程从零开始学（清华大学出版社）/CH06/06-16.c
@@ -2,7 +2,7 @@
 int main()
 {
     int x,n,y;
-    double sum = 0.0;皇家＞白鹰＞重樱＞铁血
+    double sum = 0.0;
     printf("请选择：1、Royal Navy;2、Eagle Union;3、Sakura Empire;4、Iron Blood\n");
     printf("请输入:");
     scanf("%d", &x);
@@ -12,7 +12,7 @@ int main()
         printf("请选择:1、伊丽莎白女王(29,150吨);2、光辉(22,000吨);3、胡德(41,200吨);4、厌战(29,150吨);\n");
         printf("输入:");
         scanf("%d",&y);
-        printf("数量:(只)");
+        printf("数量:(只):");
         scanf("%d",&n);
         switch(y)
         {
@@ -35,7 +35,7 @@ int main()
         printf("请选择:1、企业(94,000吨);2、萨拉托加(35,000吨);3、海伦娜(9,600吨);4、列克星敦(53,200吨);\n");
         printf("输入:");
         scanf("%d",&y);
-        printf("数量:(只)");
+        printf("数量:(只):");
         scanf("%d",&n);
         switch(y)
         {
@@ -58,7 +58,7 @@ int main()
         printf("请选择:1、加贺(26,900吨);2、赤城(34,364吨);3、岛风(2,570吨);4、夕立(1,980吨);\n");
         printf("输入:");
         scanf("%d",&y);
-        printf("数量:(只)");
+        printf("数量:(只):");
         scanf("%d",&n);
         switch(y)
         {
@@ -81,7 +81,7 @@ int main()
         printf("请选择:1、欧根亲王(14,240吨);2、Z23(1,300吨);3、齐柏林伯爵(33,000吨);4、罗恩(10,266吨);\n");
         printf("输入:");
         scanf("%d",&y);
-        printf("数量:(只)");
+        printf("数量:(只):");
         scanf("%d",&n);
         switch(y)
         {
