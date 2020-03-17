02cef3c0866963ccf0156ec8591cfc83a48a592c
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 16:14:09 +0800

    Create 07-02.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH07/07-02.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int max(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+int main()
+{
+    int x, y, z;
+    printf("请输入两个数: ");
+    scanf("%d %d", &x, &y);
+    z = max(x, y);
+    printf("max: %d\n", z);
+    return 0;
+}
