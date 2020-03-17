ea7fa1f71c742a13ec66d95b1a573f526876e074
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 12:20:10 +0800

    Create 06-11.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH06/06-11.c
@@ -0,0 +1,21 @@
+#include<stdido.h>
+int main()
+{
+   for(int i=0;i<15;i++)
+   {
+       printf("*");
+   }
+   printf("\n");
+   int j = 0;
+   while(j<4)
+   {
+       printf("*            *\n");
+       j++;
+   }
+   for(int l=0;l<15;l++)
+   {
+       printf("*");
+   }
+   printf("\n");
+   return 0;
+}
