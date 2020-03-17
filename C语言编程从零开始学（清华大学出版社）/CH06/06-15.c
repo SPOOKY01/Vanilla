0ee33af73c5e32bc8f049b1f958863dbaf94bf70
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-17 14:15:16 +0800

    Update 06-15.c

--- C语言编程从零开始学（清华大学出版社）/CH06/06-15.c
+++ C语言编程从零开始学（清华大学出版社）/CH06/06-15.c
@@ -4,24 +4,25 @@ int main()
     float a, b, c;
     printf("请输入三角形的三条边a,b,c:");
     scanf("%d %d %d",&a, &b, &c);
-    if(a==b && c==a && b==c)
+    if(a+b<=c || a+c<=b || b+c<=a)
+    {
+        printf("不构成三角形\n");
+    }
+	else if(a==b && c==a && b==c)
     {
         printf("等边三角形\n");
     }
     else if(a==b || a==c || b==c)
     {
-        if(a*a+b*b==c*c || a*a+c*c==b*b || b*b+c*c==a*a)
-        {
-            printf("等腰直角三角形\n");
-        }
-        else
-        {
-            printf("等腰三角形\n");
-        }
+        printf("等腰三角形\n");
     }
-    else if(a+b<=c|| a+c<=b || b+c<=a)
-    {
-        printf("不构成三角形");
-    }
+	else if(a*a+b*b==c*c || a*a+c*c==b*b || b*b+c*c==a*a)
+	{
+		printf("直角三角形\n");
+	}
+   	else
+	{
+		printf("一般三角形\n");
+	}
     return 0;
 }
