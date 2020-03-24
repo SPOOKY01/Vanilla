9c161afdb1e03d9d896e269241614b9f1a4cb030
吴振凯 <57130236+SPOOKY01@users.noreply.github.com>
2020-03-25 00:32:15 +0800

    Create 08-14.c

---
+++ C语言编程从零开始学（清华大学出版社）/CH08/08-14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char str[] = { "String!" };
+    for (i = 0; i < 7; i++)
+    {
+        printf("%c ", str[i]);
+    }
+    printf("\n");
+    return 0;
+}
